42c1a2193f71e6df82914b409e71d04727b8e699
NonYodying <nonthacha.yod@pea.co.th>
2024-05-19 17:46:05 +0700

    commit

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI 
 from route.endPoints import endPoints
 from route.recipe import recipeRoot
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(endPoints)
 app.include_router(recipeRoot)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
